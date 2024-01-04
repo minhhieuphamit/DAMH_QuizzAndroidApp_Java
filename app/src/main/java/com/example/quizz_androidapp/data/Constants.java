79d1ea8403c348f0475fe6e1584f184c70d8dc8a
Tung Phan <sneakiedward@gmail.com>
2024-01-05 00:14:47 +0700

    tạo question cho project (set cứng) và UI thi trắc nghiêm

---
+++ app/src/main/java/com/example/quizz_androidapp/data/Constants.java
@@ -0,0 +1,54 @@
+package com.example.quizz_androidapp.data;
+
+import com.example.quizz_androidapp.R;
+import com.example.quizz_androidapp.data.model.Question;
+
+import java.util.ArrayList;
+
+public class Constants {
+    public static final String USER_NAME = "user_name";
+    public static final String TOTAL_QUESTION = "total_question";
+    public static final String CORRECT_QUESTION = "correct_answer";
+
+    public static ArrayList<Question> getQuestions() {
+        ArrayList<Question> questionList = new ArrayList<>();
+
+        Question questionOne = new Question(
+                1, "2+2",
+                "1", "2", "3", "4",
+                4
+        );
+        questionList.add(questionOne);
+
+        Question questionTwo = new Question(
+                2, "1+2",
+                "1", "2", "3", "4",
+                3
+        );
+        questionList.add(questionTwo);
+
+        Question questionThree = new Question(
+                2, "1*2-1",
+                "1", "2", "3", "4",
+                1
+        );
+        questionList.add(questionThree);
+
+        Question questionFour = new Question(
+                2, "1+2-1",
+
+                "1", "2", "3", "4",
+                2
+        );
+        questionList.add(questionFour);
+
+        Question questionFive = new Question(
+                2, "1+2+1",
+                "1", "2", "3", "4",
+                4
+        );
+        questionList.add(questionFive);
+
+        return questionList;
+    }
+}
--- app/src/main/java/com/example/quizz_androidapp/ui/home/MathQuizActivity.java
+++ app/src/main/java/com/example/quizz_androidapp/ui/home/MathQuizActivity.java
@@ -1,26 +1,193 @@
 package com.example.quizz_androidapp.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.quizz_androidapp.R;
+import com.example.quizz_androidapp.data.Constants;
+import com.example.quizz_androidapp.data.model.Question;
 
-public class MathQuizActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MathQuizActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int mCurrentPosition = 1;
+    private ArrayList<Question> mQuestionList;
+    private int mSelectedOptionNumber = 0;
+    ProgressBar progressBar ;
+    TextView tvProgressBar ;
+    TextView tvQuestion ;
+    TextView tvOptionOne ;
+    TextView tvOptionTwo ;
+    TextView tvOptionThree ;
+    TextView tvOptionFour;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_quiz);
+        initView();
+
+        mQuestionList = Constants.getQuestions();
+        setQuestion();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        goResult();
         backToPrevious();
+    }
+
+    private void setQuestion() {
+        defaultOptionView();
+        Question question = mQuestionList.get(mCurrentPosition - 1);
+
+        tvQuestion.setText(question.getQuestion());
+
+        progressBar.setProgress(mCurrentPosition);
+        tvProgressBar.setText(mCurrentPosition + "/" + progressBar.getMax());
+
+        tvOptionOne.setText(question.getOptionOne());
+        tvOptionTwo.setText(question.getOptionTwo());
+        tvOptionThree.setText(question.getOptionThree());
+        tvOptionFour.setText(question.getOptionFour());
+
+        tvOptionOne.setOnClickListener((View.OnClickListener) this);
+        tvOptionTwo.setOnClickListener((View.OnClickListener) this);
+        tvOptionThree.setOnClickListener((View.OnClickListener) this);
+        tvOptionFour.setOnClickListener((View.OnClickListener) this);
+        btnSubmit.setOnClickListener((View.OnClickListener) this);
+
+        if (mCurrentPosition == mQuestionList.size()) {
+            btnSubmit.setText("FINISH");
+            goResult();
+        } else {
+            btnSubmit.setText("SUBMIT");
+        }
+    }
+
+    private void defaultOptionView() {
+        ArrayList<TextView> options = new ArrayList<>();
+        if (tvOptionOne != null) {
+            options.add(tvOptionOne);
+        }
+        if (tvOptionTwo != null) {
+            options.add(tvOptionTwo);
+        }
+        if (tvOptionThree != null) {
+            options.add(tvOptionThree);
+        }
+        if (tvOptionFour != null) {
+            options.add(tvOptionFour);
+        }
+        for (TextView i : options) {
+            i.setTextColor(Color.parseColor("#7A8089"));
+            i.setTypeface(Typeface.DEFAULT);
+            i.setBackground(ContextCompat.getDrawable(this, R.drawable.option_choice));
+        }
+    }
+
+    private void selectedOptionView(TextView tv, int selectedOptionNumber) {
+        defaultOptionView();
+        mSelectedOptionNumber = selectedOptionNumber;
+        tv.setTextColor(Color.parseColor("#363A43"));
+        tv.setTypeface(tv.getTypeface(), Typeface.BOLD);
+        tv.setBackground(ContextCompat.getDrawable(this, R.drawable.option_choice_selected));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v != null) {
+            int viewId = v.getId();
+
+            if (viewId == R.id.tv_optionOne) {
+                if (tvOptionOne != null) {
+                    selectedOptionView(tvOptionOne, 1);
+                }
+            } else if (viewId == R.id.tv_optionTwo) {
+                if (tvOptionTwo != null) {
+                    selectedOptionView(tvOptionTwo, 2);
+                }
+            } else if (viewId == R.id.tv_optionThree) {
+                if (tvOptionThree != null) {
+                    selectedOptionView(tvOptionThree, 3);
+                }
+            } else if (viewId == R.id.tv_optionFour) {
+                if (tvOptionFour != null) {
+                    selectedOptionView(tvOptionFour, 4);
+                }
+            } else if (viewId == R.id.btn_submit) {
+                if (mSelectedOptionNumber == 0) {
+                    mCurrentPosition++;
+                    if (mCurrentPosition <= mQuestionList.size()) {
+                        setQuestion();
+                    } else {
+                        Toast.makeText(this, "You're Done", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Question question = mQuestionList.get(mCurrentPosition - 1);
+                    if (question.getCorrectAnswer() != mSelectedOptionNumber) {
+                        answerView(mSelectedOptionNumber, R.drawable.wrong_option_choice);
+                    }
+                    answerView(question.getCorrectAnswer(), R.drawable.correct_option_choice);
+
+                    if (mCurrentPosition == mQuestionList.size()) {
+                        btnSubmit.setText("FINISH");
+                        goResult();
+                    } else {
+                        btnSubmit.setText("NEXT");
+                    }
+                    mSelectedOptionNumber = 0;
+                }
+            }
+        }
+    }
+
+    private void answerView(int answer, int drawableView) {
+        switch (answer) {
+            case 1:
+                if (tvOptionOne != null) {
+                    tvOptionOne.setBackground(ContextCompat.getDrawable(this, drawableView));
+                }
+                break;
+            case 2:
+                if (tvOptionTwo != null) {
+                    tvOptionTwo.setBackground(ContextCompat.getDrawable(this, drawableView));
+                }
+                break;
+            case 3:
+                if (tvOptionThree != null) {
+                    tvOptionThree.setBackground(ContextCompat.getDrawable(this, drawableView));
+                }
+                break;
+            case 4:
+                if (tvOptionFour != null) {
+                    tvOptionFour.setBackground(ContextCompat.getDrawable(this, drawableView));
+                }
+                break;
+        }
+    }
+
+    public void initView(){
+        progressBar = findViewById(R.id.progressBar);
+        tvProgressBar = findViewById(R.id.tv_progressBar);
+        tvQuestion = findViewById(R.id.tv_question);
+        tvOptionOne = findViewById(R.id.tv_optionOne);
+        tvOptionTwo = findViewById(R.id.tv_optionTwo);
+        tvOptionThree = findViewById(R.id.tv_optionThree);
+        tvOptionFour = findViewById(R.id.tv_optionFour);
+        btnSubmit = findViewById(R.id.btn_submit);
     }
 
     private void backToPrevious(){
