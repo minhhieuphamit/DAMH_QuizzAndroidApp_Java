68847174ec3ea8984a54b7c0dd62fee5e648ebf1
Tung Phan <sneakiedward@gmail.com>
2024-01-05 14:01:12 +0700

    xây cấu trúc api và refactor math

---
+++ app/src/main/java/com/example/quizz_androidapp/api/APIInterface.java
@@ -0,0 +1,13 @@
+package com.example.quizz_androidapp.api;
+
+import java.util.UUID;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+public interface APIInterface {
+
+}
---
+++ app/src/main/java/com/example/quizz_androidapp/data/model/User.java
@@ -0,0 +1,4 @@
+package com.example.quizz_androidapp.data.model;
+
+public class User {
+}
